59c56051ac7febe9b14602480e8ed7cae1b2dad7
eli <elizabeth.johana.gonzalez@gmail.com>
2020-09-30 17:02:14 -0300

    updated colors

--- red_sequence_find.py
+++ red_sequence_find.py
@@ -4,6 +4,7 @@ sys.path.append('/home/eli/lens_codes_v3.7')
 import numpy as np
 from pylab import *
 from astropy.io import fits
+from scipy.optimize import curve_fit
 
 folder_RM = '/home/eli/Documentos/Astronomia/proyectos/DES-Y1/catalogs/'
 folder_Mice = '/home/eli/Documentos/Astronomia/proyectos/MICEv1.0/catalogs/'
@@ -14,10 +15,10 @@ halos    = fits.open(folder_Mice+'MICEv1.0_halo_cat.fits')[1].data
 
 # MICE PARAMETERS
 
-mg_h = halos.g_des_realization
-mr_h = halos.r_des_realization
-mi_h = halos.i_des_realization
-mz_h = halos.z_des_realization
+mg_h = halos.g_des_true
+mr_h = halos.r_des_true
+mi_h = halos.i_des_true
+mz_h = halos.z_des_true
   
 z_h = halos.z_v
 lMH  = halos.log_m
@@ -45,14 +46,24 @@ lMC = np.log10(M0 * (Lambda/40.)**Fl * ((1. + z_c)/(1. + 0.35))**Gz)
 mc = (z_c > 0.2)*(z_c < 0.7)
 mh = (z_h > 0.2)*(z_h < 0.7)*(lMH > 14.)
 
-plt.hist(lMC[mc],histtype='step', label = 'redMaPPer')  
-plt.hist(lMH[mh],histtype='step',label = 'MICE')
+mz35_c = (z_c >= 0.35)
+mz35_h = (z_h >= 0.35)
+
+color_c         = mg_c - mr_c
+color_c[mz35_c] = (mr_c - mi_c)[mz35_c]
+
+color_h         = mg_h - mr_h
+color_h[mz35_h] = (mr_h - mi_h)[mz35_h]
+
+
+plt.hist(lMC[mc],np.linspace(14,15.5,20),histtype='step', label = 'redMaPPer')  
+plt.hist(lMH[mh],np.linspace(14,15.5,20),histtype='step',label = 'MICE')
 plt.legend()
 plt.xlabel('log(M)')  
 
 plt.figure()
-plt.hist(z_c[mc],histtype='step', label = 'redMaPPer')  
-plt.hist(z_h[mh],histtype='step',label = 'MICE')
+plt.hist(z_c[mc],np.linspace(0.2,0.7,20),histtype='step', label = 'redMaPPer')  
+plt.hist(z_h[mh],np.linspace(0.2,0.7,20),histtype='step',label = 'MICE')
 plt.legend()
 plt.xlabel('z')  
 
@@ -61,18 +72,21 @@ for j in range(50):
 
     zmin = 0.2+0.01*j
     print(str('%.2f' % zmin))
-    mz_c = (z_c > zmin)*(z_c < (zmin + 0.01))
-    mz_h = (z_h > zmin)*(z_h < (zmin + 0.01))
+    mz_c = (z_c >= zmin)*(z_c < (zmin + 0.01))
+    mz_h = (z_h >= zmin)*(z_h < (zmin + 0.01))
    
     
     plt.figure()
-    plt.plot(mr_h[mz_h],(mg_h-mr_h)[mz_h],'C7.',label = 'MICE')
-    plt.plot(mr_c[mz_c*mc],(mg_c-mr_c)[mz_c*mc],'.', label = 'redMaPPer')  
-    plt.plot(mr_h[mz_h*mh],(mg_h-mr_h)[mz_h*mh],'.',label = 'MICE filtered')
+    plt.plot(mr_h[mz_h],color_h[mz_h],'C7.',label = 'MICE')
+    plt.plot(mr_c[mz_c],color_c[mz_c],'.', label = 'redMaPPer')  
+    plt.plot(mr_h[mz_h*mh],color_h[mz_h*mh],'.',label = 'MICE - lMH > 14')
     plt.legend()
     plt.xlabel('mag_r')
-    plt.ylabel('g-r')
-    plt.savefig('/home/eli/Documentos/Astronomia/proyectos/MICEv1.0/RS_test_plots/rs_z'+str('%.2f' % zmin)+'2.png')
+    if zmin < 0.35:
+        plt.ylabel('g-r')
+    else:
+        plt.ylabel('r-i')
+    plt.savefig('/home/eli/Documentos/Astronomia/proyectos/MICEv1.0/RS_test_plots/rs_z'+str('%.2f' % zmin)+'_true.png')
 
 
 def gauss(x, a,mu,sigma):
@@ -81,9 +95,9 @@ def gauss(x, a,mu,sigma):
 mz_h = (z_h > 0.2)*(z_h < 0.21)
 mz_c = (z_c > 0.2)*(z_c < 0.21)
     
-color2 = (mg_h-mr_h)[mz_h]
-n,c    = np.histogram(color2,15)      
-c      = (c+(c[1]-c[0])*0.5)[:-1]
+color_hf = color_h[mz_h]
+n,c      = np.histogram(color_hf,15)      
+c        = (c+(c[1]-c[0])*0.5)[:-1]
 
 err   = np.ones(len(c))
 
@@ -92,13 +106,13 @@ a         = fit_gauss[0][0]
 mu        = fit_gauss[0][1]
 sigma     = abs(fit_gauss[0][2])
 
-color_c = (mg_c-mr_c)[mz_c*mc]
-n_c,c_c    = np.histogram(color_c,15)      
+color_cf = color_c[mz_c]
+n_c,c_c    = np.histogram(color_cf,15)      
 c_c      = (c_c+(c_c[1]-c_c[0])*0.5)[:-1]
 
 plt.figure()
-plt.plot(mr_h[mz_h],color2,'C7.')
-plt.plot(mr_c[mz_c*mc],color_c,'.')
+plt.plot(mr_h[mz_h],color_hf,'C7.')
+plt.plot(mr_c[mz_c*mc],color_cf,'.')
 
 
 

0924a7cb035aaf82fefb1b58bfbb341c3bb7b0e7
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2020-12-14 18:46:33 -0300

    update mice

--- analysis_profile.py
+++ analysis_profile.py
@@ -13,13 +13,441 @@ G    = G.value;   # Gravitational constant (m3.kg-1.s-2)
 pc   = pc.value # 1 pc (m)
 Msun = M_sun.value # Solar mass (kg)
 
-
-
-def plt_profile(mbin,ax,ax1,ax2,ax3,RIN,ROUT,mv):
+def plt_profile_compare(samp1,samp2,ax,ax1,ax2,ax3,RIN,ROUT,mv1,mv2):
     
     folder = '/home/elizabeth/Documentos/proyectos/HALO-SHAPE/MICEv'+str(mv)+'.0/profiles/'
 
-    p_name = 'profile_bin_'+str(int(mbin))+'.fits'
+
+    p_name = 'profile_'+samp1+'.fits'
+    profile = fits.open(folder+p_name)
+
+    print(p_name)
+    
+    # '''
+    h   = profile[0].header
+    p   = profile[1].data
+    cov = profile[2].data
+    
+    cosmo = LambdaCDM(H0=100*h['hcosmo'], Om0=0.25, Ode0=0.75)
+    '''
+    
+    h = profile[1].header
+    p = profile[1].data
+    '''
+    micev = str(h['MICE version'])
+    
+    zmean = h['z_mean']
+    q  = h['q2d_mean']
+    qr = h['q2dr_mean']
+    
+    e = (1-q)/(1+q)
+    er = (1-qr)/(1+qr)
+    
+    H        = cosmo.H(zmean).value/(1.0e3*pc) #H at z_pair s-1 
+    roc      = (3.0*(H**2.0))/(8.0*np.pi*G) #critical density at z_pair (kg.m-3)
+    roc_mpc  = roc*((pc*1.0e6)**3.0)
+    
+    
+    ndots = p.shape[0]
+    
+    
+    GT  = p.GAMMA_Tcos
+    GTr = p.GAMMA_Tcos_reduced
+    GTc = p.GAMMA_Tcos_control
+    
+    GX  = p.GAMMA_Xsin
+    GXr = p.GAMMA_Xsin_reduced
+    GXc = p.GAMMA_Xsin_control
+    
+    # '''
+    CovDS  = cov.COV_ST.reshape(len(GT),len(GT))
+    
+    CovGT  = cov.COV_GT.reshape(len(GT),len(GT))
+    CovGTr = cov.COV_GT_reduced.reshape(len(GT),len(GT))
+    CovGTc = cov.COV_GT_control.reshape(len(GT),len(GT))
+    
+    CovGX  = cov.COV_GX.reshape(len(GT),len(GT))
+    CovGXr = cov.COV_GX_reduced.reshape(len(GT),len(GT))
+    CovGXc = cov.COV_GX_control.reshape(len(GT),len(GT))
+
+    # FIT MONOPOLE
+    rplot = np.arange(0.1,5,0.05)
+    
+    nfw    = Delta_Sigma_fit(p.Rp,p.DSigma_T,np.diag(CovDS),zmean,cosmo,True)
+    gt,gx   = GAMMA_components(rplot,zmean,ellip=e,M200 =nfw.M200,c200 = nfw.c200,cosmo=cosmo)
+    gtr,gxr = GAMMA_components(rplot,zmean,ellip=er,M200 =nfw.M200,c200 = nfw.c200,cosmo=cosmo)
+    
+    mass = str(np.round(np.log10(nfw.M200),1))
+    
+    
+    
+    ax.plot(1000,1000,'w.' ,label='$\log M_{200}=$'+mass)
+    ax1.plot(1000,1000,'w.',label='$\log M_{200}=$'+mass)
+    ax2.plot(1000,1000,'w.',label='$\log M_{200}=$'+mass)
+    ax3.plot(1000,1000,'w.',label='$\log M_{200}=$'+mass)
+
+    
+    ax1.legend()
+    ax2.legend()
+    
+    
+    ax.plot(p.Rp,p.DSigma_T,'C0')
+    ax.plot(nfw.xplot,nfw.yplot,'C0',label=samp1)
+    ax.fill_between(p.Rp,p.DSigma_T+np.diag(CovDS),p.DSigma_T-np.diag(CovDS),color='C0',alpha=0.2)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax.set_ylim(2,200)
+    ax.set_xlim(0.1,5)
+    ax.xaxis.set_ticks([0.1,1,3])
+    ax.set_xticklabels([0.1,1,3])
+    ax.yaxis.set_ticks([5,10,100])
+    ax.set_yticklabels([5,10,100])
+    ax.legend()
+    
+    # ax1.plot(RMt[0]*0.7,RMt[1]/0.7,'k',label='redMaPPer')
+    # ax1.errorbar(RMt[0]*0.7,RMt[1]/0.7,yerr=RMt[2]/0.7,fmt = 'none',ecolor='0.5')
+    
+    ax1.plot(p.Rp,GT,'C0',label = samp1)
+    ax1.plot(rplot,gt,'C0')
+
+    ax1.fill_between(p.Rp,GT+np.diag(CovGT),GT-np.diag(CovGT),color='C0',alpha=0.2)
+    ax1.set_xscale('log')
+    ax1.set_yscale('log')
+    ax1.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax1.set_ylim(1,100)
+    ax1.set_xlim(0.1,5)
+    ax1.xaxis.set_ticks([0.1,1,3])
+    ax1.set_xticklabels([0.1,1,3])
+    ax1.yaxis.set_ticks([0.3,10,100])
+    ax1.set_yticklabels([0.3,10,100])
+    
+    # ax2.plot(RMt[0]*0.7,RMt[3]/0.7,'k',label='redMaPPer')
+    # ax2.errorbar(RMt[0]*0.7,RMt[3]/0.7,yerr=RMt[4]/0.7,fmt = 'none',ecolor='0.5')
+    
+    ax2.plot([0,5],[0,0],'C7')
+    ax2.plot(p.Rp,GX,'C0')
+    ax2.plot(rplot,gx,'C0')
+    
+    ax2.fill_between(p.Rp,GX+np.diag(CovGX),GX-np.diag(CovGX),color='C0',alpha=0.2)
+    ax2.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax2.set_xscale('log')
+    ax2.set_xlim(0.1,5)
+    ax2.set_ylim(-20,20)
+    ax2.xaxis.set_ticks([0.1,1,3])
+    ax2.set_xticklabels([0.1,1,3])
+    
+    
+    ax3.plot([0,5],[0,0],'C7')
+    ax3.plot(p.Rp,GTc,'C0', label = 'GT control')
+    ax3.plot(p.Rp,GXc,'C0--', label = 'GX control')
+    ax3.fill_between(p.Rp,GXc+np.diag(CovGXc),GXc-np.diag(CovGXc),color='C0',alpha=0.2)
+    ax3.fill_between(p.Rp,GTc+np.diag(CovGTc),GTc-np.diag(CovGTc),color='C0',alpha=0.2)
+    ax3.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax3.set_xscale('log')
+    ax3.set_xlim(0.1,5)
+    ax3.set_ylim(-10,7)
+    ax3.xaxis.set_ticks([0.1,1,3])
+    ax3.set_xticklabels([0.1,1,3])
+    ax3.legend()
+
+
+    folder = '/home/elizabeth/Documentos/proyectos/HALO-SHAPE/MICEv'+str(mv2)+'.0/profiles/'
+
+
+    p_name = 'profile_'+samp2+'.fits'
+    profile = fits.open(folder+p_name)
+
+    print(p_name)
+    
+    # '''
+    h   = profile[0].header
+    p   = profile[1].data
+    cov = profile[2].data
+    
+    cosmo = LambdaCDM(H0=100*h['hcosmo'], Om0=0.25, Ode0=0.75)
+    '''
+    
+    h = profile[1].header
+    p = profile[1].data
+    '''
+    micev = str(h['MICE version'])
+    
+    zmean = h['z_mean']
+    q  = h['q2d_mean']
+    qr = h['q2dr_mean']
+    
+    e = (1-q)/(1+q)
+    er = (1-qr)/(1+qr)
+    
+    H        = cosmo.H(zmean).value/(1.0e3*pc) #H at z_pair s-1 
+    roc      = (3.0*(H**2.0))/(8.0*np.pi*G) #critical density at z_pair (kg.m-3)
+    roc_mpc  = roc*((pc*1.0e6)**3.0)
+    
+    
+    ndots = p.shape[0]
+    
+    
+    GT  = p.GAMMA_Tcos
+    GTr = p.GAMMA_Tcos_reduced
+    GTc = p.GAMMA_Tcos_control
+    
+    GX  = p.GAMMA_Xsin
+    GXr = p.GAMMA_Xsin_reduced
+    GXc = p.GAMMA_Xsin_control
+    
+    # '''
+    CovDS  = cov.COV_ST.reshape(len(GT),len(GT))
+    
+    CovGT  = cov.COV_GT.reshape(len(GT),len(GT))
+    CovGTr = cov.COV_GT_reduced.reshape(len(GT),len(GT))
+    CovGTc = cov.COV_GT_control.reshape(len(GT),len(GT))
+    
+    CovGX  = cov.COV_GX.reshape(len(GT),len(GT))
+    CovGXr = cov.COV_GX_reduced.reshape(len(GT),len(GT))
+    CovGXc = cov.COV_GX_control.reshape(len(GT),len(GT))
+
+    # FIT MONOPOLE
+    rplot = np.arange(0.1,5,0.05)
+    
+    nfw    = Delta_Sigma_fit(p.Rp,p.DSigma_T,np.diag(CovDS),zmean,cosmo,True)
+    gt,gx   = GAMMA_components(rplot,zmean,ellip=e,M200 =nfw.M200,c200 = nfw.c200,cosmo=cosmo)
+    gtr,gxr = GAMMA_components(rplot,zmean,ellip=er,M200 =nfw.M200,c200 = nfw.c200,cosmo=cosmo)
+    
+    mass = str(np.round(np.log10(nfw.M200),1))
+    
+    
+    
+    ax.plot(1000,1000,'w.' ,label='$\log M_{200}=$'+mass)
+    ax1.plot(1000,1000,'w.',label='$\log M_{200}=$'+mass)
+    ax2.plot(1000,1000,'w.',label='$\log M_{200}=$'+mass)
+    ax3.plot(1000,1000,'w.',label='$\log M_{200}=$'+mass)
+
+    
+    
+    
+    ax.plot(p.Rp,p.DSigma_T,'C1')
+    ax.plot(nfw.xplot,nfw.yplot,'C1',label=samp2)
+    ax.fill_between(p.Rp,p.DSigma_T+np.diag(CovDS),p.DSigma_T-np.diag(CovDS),color='C1',alpha=0.2)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax.set_ylabel(r'$\Delta\Sigma$')
+    ax.set_ylim(2,200)
+    ax.set_xlim(0.1,5)
+    ax.xaxis.set_ticks([0.1,1,3])
+    ax.set_xticklabels([0.1,1,3])
+    ax.yaxis.set_ticks([5,10,100])
+    ax.set_yticklabels([5,10,100])
+    ax.legend()
+    
+    # ax1.plot(RMt[0]*0.7,RMt[1]/0.7,'k',label='redMaPPer')
+    # ax1.errorbar(RMt[0]*0.7,RMt[1]/0.7,yerr=RMt[2]/0.7,fmt = 'none',ecolor='0.5')
+    
+    ax1.plot(p.Rp,GT,'C1',label = samp2)
+    ax1.plot(rplot,gt,'C1')
+
+    ax1.fill_between(p.Rp,GT+np.diag(CovGT),GT-np.diag(CovGT),color='C1',alpha=0.2)
+    ax1.set_xscale('log')
+    ax1.set_yscale('log')
+    ax1.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax1.set_ylabel(r'$\Gamma_T$')
+    ax1.set_ylim(1,100)
+    ax1.set_xlim(0.1,5)
+    ax1.xaxis.set_ticks([0.1,1,3])
+    ax1.set_xticklabels([0.1,1,3])
+    ax1.yaxis.set_ticks([0.3,10,100])
+    ax1.set_yticklabels([0.3,10,100])
+
+
+    
+    # ax2.plot(RMt[0]*0.7,RMt[3]/0.7,'k',label='redMaPPer')
+    # ax2.errorbar(RMt[0]*0.7,RMt[3]/0.7,yerr=RMt[4]/0.7,fmt = 'none',ecolor='0.5')
+    
+    ax2.plot([0,5],[0,0],'C7')
+    ax2.plot(p.Rp,GX,'C1')
+    ax2.plot(rplot,gx,'C1')
+    
+    ax2.fill_between(p.Rp,GX+np.diag(CovGX),GX-np.diag(CovGX),color='C1',alpha=0.2)
+    ax2.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax2.set_ylabel(r'$\Gamma_\times$')
+    ax2.set_xscale('log')
+    ax2.set_xlim(0.1,5)
+    ax2.set_ylim(-20,20)
+    ax2.xaxis.set_ticks([0.1,1,3])
+    ax2.set_xticklabels([0.1,1,3])
+
+    ax1.legend()
+    ax2.legend()
+    
+    
+    ax3.plot([0,5],[0,0],'C7')
+    ax3.plot(p.Rp,GTc,'C1', label = 'GT control')
+    ax3.plot(p.Rp,GXc,'C1--', label = 'GX control')
+    ax3.fill_between(p.Rp,GXc+np.diag(CovGXc),GXc-np.diag(CovGXc),color='C1',alpha=0.2)
+    ax3.fill_between(p.Rp,GTc+np.diag(CovGTc),GTc-np.diag(CovGTc),color='C1',alpha=0.2)
+    ax3.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax3.set_xscale('log')
+    ax3.set_xlim(0.1,5)
+    ax3.set_ylim(-10,7)
+    ax3.xaxis.set_ticks([0.1,1,3])
+    ax3.set_xticklabels([0.1,1,3])
+    ax3.legend()
+
+def plt_profile_wofit(samp,ax,ax1,ax2,ax3,RIN,ROUT,mv):
+    
+    folder = '/home/elizabeth/Documentos/proyectos/HALO-SHAPE/MICEv'+str(mv)+'.0/profiles/'
+
+
+    p_name = 'profile_'+samp+'.fits'
+    profile = fits.open(folder+p_name)
+
+    print(p_name)
+    
+    # '''
+    h   = profile[0].header
+    p   = profile[1].data
+    cov = profile[2].data
+    
+    cosmo = LambdaCDM(H0=100*h['hcosmo'], Om0=0.25, Ode0=0.75)
+    '''
+    
+    h = profile[1].header
+    p = profile[1].data
+    '''
+    micev = str(h['MICE version'])
+    
+    zmean = h['z_mean']
+    q  = h['q2d_mean']
+    qr = h['q2dr_mean']
+    
+    e = (1-q)/(1+q)
+    er = (1-qr)/(1+qr)
+    
+    H        = cosmo.H(zmean).value/(1.0e3*pc) #H at z_pair s-1 
+    roc      = (3.0*(H**2.0))/(8.0*np.pi*G) #critical density at z_pair (kg.m-3)
+    roc_mpc  = roc*((pc*1.0e6)**3.0)
+    
+    
+    ndots = p.shape[0]
+    
+    
+    GT  = p.GAMMA_Tcos
+    GTr = p.GAMMA_Tcos_reduced
+    GTc = p.GAMMA_Tcos_control
+    
+    GX  = p.GAMMA_Xsin
+    GXr = p.GAMMA_Xsin_reduced
+    GXc = p.GAMMA_Xsin_control
+    
+    # '''
+    CovDS  = cov.COV_ST.reshape(len(GT),len(GT))
+    
+    CovGT  = cov.COV_GT.reshape(len(GT),len(GT))
+    CovGTr = cov.COV_GT_reduced.reshape(len(GT),len(GT))
+    CovGTc = cov.COV_GT_control.reshape(len(GT),len(GT))
+    
+    CovGX  = cov.COV_GX.reshape(len(GT),len(GT))
+    CovGXr = cov.COV_GX_reduced.reshape(len(GT),len(GT))
+    CovGXc = cov.COV_GX_control.reshape(len(GT),len(GT))
+
+    # FIT MONOPOLE
+    rplot = np.arange(0.1,5,0.05)
+    
+    nfw    = Delta_Sigma_fit(p.Rp,p.DSigma_T,np.diag(CovDS),zmean,cosmo,True)
+    gt,gx   = GAMMA_components(rplot,zmean,ellip=e,M200 =nfw.M200,c200 = nfw.c200,cosmo=cosmo)
+    gtr,gxr = GAMMA_components(rplot,zmean,ellip=er,M200 =nfw.M200,c200 = nfw.c200,cosmo=cosmo)
+    
+    mass = str(np.round(np.log10(nfw.M200),1))
+    
+    
+    
+    ax.plot(1000,1000,'w.' ,label='$\log M_{200}=$'+mass)
+    ax1.plot(1000,1000,'w.',label='$\log M_{200}=$'+mass)
+    ax2.plot(1000,1000,'w.',label='$\log M_{200}=$'+mass)
+    ax3.plot(1000,1000,'w.',label='$\log M_{200}=$'+mass)
+
+    
+    ax1.legend()
+    ax2.legend()
+    
+    ax.set_title('MICE v'+micev+'.0')
+    ax1.set_title('MICE v'+micev+'.0')
+    ax2.set_title('MICE v'+micev+'.0')
+    ax3.set_title('MICE v'+micev+'.0')
+    
+    ax.plot(p.Rp,p.DSigma_T,'C1')
+    ax.plot(nfw.xplot,nfw.yplot,'C3',label='fited nfw')
+    ax.fill_between(p.Rp,p.DSigma_T+np.diag(CovDS),p.DSigma_T-np.diag(CovDS),color='C1',alpha=0.2)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax.set_ylim(2,200)
+    ax.set_xlim(0.1,5)
+    ax.xaxis.set_ticks([0.1,1,3])
+    ax.set_xticklabels([0.1,1,3])
+    ax.yaxis.set_ticks([5,10,100])
+    ax.set_yticklabels([5,10,100])
+    ax.legend()
+    
+    # ax1.plot(RMt[0]*0.7,RMt[1]/0.7,'k',label='redMaPPer')
+    # ax1.errorbar(RMt[0]*0.7,RMt[1]/0.7,yerr=RMt[2]/0.7,fmt = 'none',ecolor='0.5')
+    
+    ax1.plot(p.Rp,GT,'C4',label = 'standard')
+    ax1.plot(p.Rp,GTr,'C0--',label = 'reduced')
+    ax1.plot(rplot,gt,'C3')
+    ax1.plot(rplot,gtr,'C3--')
+
+    ax1.fill_between(p.Rp,GT+np.diag(CovGT),GT-np.diag(CovGT),color='C4',alpha=0.2)
+    ax1.fill_between(p.Rp,GTr+np.diag(CovGTr),GTr-np.diag(CovGTr),color='C0',alpha=0.2)
+    ax1.set_xscale('log')
+    ax1.set_yscale('log')
+    ax1.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax1.set_ylim(1,100)
+    ax1.set_xlim(0.1,5)
+    ax1.xaxis.set_ticks([0.1,1,3])
+    ax1.set_xticklabels([0.1,1,3])
+    ax1.yaxis.set_ticks([0.3,10,100])
+    ax1.set_yticklabels([0.3,10,100])
+    
+    # ax2.plot(RMt[0]*0.7,RMt[3]/0.7,'k',label='redMaPPer')
+    # ax2.errorbar(RMt[0]*0.7,RMt[3]/0.7,yerr=RMt[4]/0.7,fmt = 'none',ecolor='0.5')
+    
+    ax2.plot([0,5],[0,0],'C7')
+    ax2.plot(p.Rp,GX,'C2')
+    ax2.plot(p.Rp,GXr,'C5--')
+    ax2.plot(rplot,gx,'C3')
+    ax2.plot(rplot,gxr,'C3--')
+    
+    ax2.fill_between(p.Rp,GX+np.diag(CovGX),GX-np.diag(CovGX),color='C2',alpha=0.2)
+    ax2.fill_between(p.Rp,GXr+np.diag(CovGXr),GXr-np.diag(CovGXr),color='C5',alpha=0.2)
+    ax2.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax2.set_xscale('log')
+    ax2.set_xlim(0.1,5)
+    ax2.set_ylim(-20,20)
+    ax2.xaxis.set_ticks([0.1,1,3])
+    ax2.set_xticklabels([0.1,1,3])
+    
+    
+    ax3.plot([0,5],[0,0],'C7')
+    ax3.plot(p.Rp,GTc,'k', label = 'GT control')
+    ax3.plot(p.Rp,GXc,'C8--', label = 'GX control')
+    ax3.fill_between(p.Rp,GXc+np.diag(CovGXc),GXc-np.diag(CovGXc),color='C8',alpha=0.2)
+    ax3.fill_between(p.Rp,GTc+np.diag(CovGTc),GTc-np.diag(CovGTc),color='C7',alpha=0.2)
+    ax3.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax3.set_xscale('log')
+    ax3.set_xlim(0.1,5)
+    ax3.set_ylim(-10,7)
+    ax3.xaxis.set_ticks([0.1,1,3])
+    ax3.set_xticklabels([0.1,1,3])
+    ax3.legend()
+
+
+def plt_profile(samp,ax,ax1,ax2,ax3,RIN,ROUT,mv):
+    
+    folder = '/home/elizabeth/Documentos/proyectos/HALO-SHAPE/MICEv'+str(mv)+'.0/profiles/'
+
+    p_name = 'profile_'+samp+'.fits'
     profile = fits.open(folder+p_name)
 
     print(p_name)
@@ -130,9 +558,9 @@ def plt_profile(mbin,ax,ax1,ax2,ax3,RIN,ROUT,mv):
     ax.set_yscale('log')
     ax.set_xlabel('r [$h^{-1}$ Mpc]')
     ax.set_ylim(2,200)
-    ax.set_xlim(0.4,5)
-    ax.xaxis.set_ticks([0.4,1,3])
-    ax.set_xticklabels([0.4,1,3])
+    ax.set_xlim(0.1,5)
+    ax.xaxis.set_ticks([0.1,1,3])
+    ax.set_xticklabels([0.1,1,3])
     ax.yaxis.set_ticks([5,10,100])
     ax.set_yticklabels([5,10,100])
     ax.legend()
@@ -157,9 +585,9 @@ def plt_profile(mbin,ax,ax1,ax2,ax3,RIN,ROUT,mv):
     ax1.set_yscale('log')
     ax1.set_xlabel('r [$h^{-1}$ Mpc]')
     ax1.set_ylim(1,100)
-    ax1.set_xlim(0.4,5)
-    ax1.xaxis.set_ticks([0.4,1,3])
-    ax1.set_xticklabels([0.4,1,3])
+    ax1.set_xlim(0.1,5)
+    ax1.xaxis.set_ticks([0.1,1,3])
+    ax1.set_xticklabels([0.1,1,3])
     ax1.yaxis.set_ticks([0.3,10,100])
     ax1.set_yticklabels([0.3,10,100])
     
@@ -181,10 +609,10 @@ def plt_profile(mbin,ax,ax1,ax2,ax3,RIN,ROUT,mv):
     ax2.fill_between(p.Rp,GXr+np.diag(CovGXr),GXr-np.diag(CovGXr),color='C5',alpha=0.2)
     ax2.set_xlabel('r [$h^{-1}$ Mpc]')
     ax2.set_xscale('log')
-    ax2.set_xlim(0.4,5)
+    ax2.set_xlim(0.1,5)
     ax2.set_ylim(-10,10)
-    ax2.xaxis.set_ticks([0.4,1,3])
-    ax2.set_xticklabels([0.4,1,3])
+    ax2.xaxis.set_ticks([0.1,1,3])
+    ax2.set_xticklabels([0.1,1,3])
     
     
     ax3.plot([0,5],[0,0],'C7')
@@ -194,10 +622,10 @@ def plt_profile(mbin,ax,ax1,ax2,ax3,RIN,ROUT,mv):
     ax3.fill_between(p.Rp,GTc+np.diag(CovGTc),GTc-np.diag(CovGTc),color='C7',alpha=0.2)
     ax3.set_xlabel('r [$h^{-1}$ Mpc]')
     ax3.set_xscale('log')
-    ax3.set_xlim(0.4,5)
+    ax3.set_xlim(0.1,5)
     ax3.set_ylim(-10,7)
-    ax3.xaxis.set_ticks([0.4,1,3])
-    ax3.set_xticklabels([0.4,1,3])
+    ax3.xaxis.set_ticks([0.1,1,3])
+    ax3.set_xticklabels([0.1,1,3])
     ax3.legend()
 
 folder = '/home/elizabeth/Documentos/proyectos/HALO-SHAPE/MICEv'+str(mv)+'.0/profiles/'
@@ -208,12 +636,13 @@ f2, ax2 = plt.subplots()
 f3, ax3 = plt.subplots()
 
 
-plt_profile(140,ax,ax1,ax2,ax3,RIN,ROUT,mv)
+# plt_profile_micec(samp,ax,ax1,ax2,ax3,RIN,ROUT)
+plt_profile_compare('140_144_zlow','140_144_zhigh',ax,ax1,ax2,ax3,RIN,ROUT,2,2)
 
-f.savefig(folder+'DSigma_'+str(RIN)+'_'+str(ROUT)+'.png')
-f1.savefig(folder+'GT_'+str(RIN)+'_'+str(ROUT)+'.png')
-f2.savefig(folder+'GX_'+str(RIN)+'_'+str(ROUT)+'.png')
-f3.savefig(folder+'control.png')
+f.savefig(folder+'comparison/'+'DSigma_'+samp+'_'+str(RIN)+'_'+str(ROUT)+'.png')
+f1.savefig(folder+'comparison/'+'GT_'+samp+'_'+str(RIN)+'_'+str(ROUT)+'.png')
+f2.savefig(folder+'comparison/'+'GX_'+samp+'_'+str(RIN)+'_'+str(ROUT)+'.png')
+f3.savefig(folder+'comparison/'+'control'+samp+'.png')
 
 
 

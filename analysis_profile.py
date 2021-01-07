c3b1d07954bffecc8473824ac4c8db136b90a718
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2021-01-07 19:11:05 -0300

    fit monopole misscentred

--- analysis_profile.py
+++ analysis_profile.py
@@ -381,6 +381,7 @@ def plt_profile_wofit(samp,ax,ax1,ax2,ax3,RIN,ROUT,mv):
     ax.fill_between(p.Rp,p.DSigma_T+np.diag(CovDS),p.DSigma_T-np.diag(CovDS),color='C1',alpha=0.2)
     ax.set_xscale('log')
     ax.set_yscale('log')
+    ax.set_ylabel(r'$\Delta\Sigma$')
     ax.set_xlabel('r [$h^{-1}$ Mpc]')
     ax.set_ylim(2,200)
     ax.set_xlim(0.1,10)
@@ -403,6 +404,7 @@ def plt_profile_wofit(samp,ax,ax1,ax2,ax3,RIN,ROUT,mv):
     ax1.set_xscale('log')
     ax1.set_yscale('log')
     ax1.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax1.set_ylabel(r'$\Gamma_T$')
     ax1.set_ylim(1,100)
     ax1.set_xlim(0.1,10)
     ax1.xaxis.set_ticks([0.1,1,5,7])
@@ -422,6 +424,7 @@ def plt_profile_wofit(samp,ax,ax1,ax2,ax3,RIN,ROUT,mv):
     ax2.fill_between(p.Rp,GX+np.diag(CovGX),GX-np.diag(CovGX),color='C2',alpha=0.2)
     ax2.fill_between(p.Rp,GXr+np.diag(CovGXr),GXr-np.diag(CovGXr),color='C5',alpha=0.2)
     ax2.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax2.set_ylabel(r'$\Gamma_\times$')
     ax2.set_xscale('log')
     ax2.set_xlim(0.1,10)
     ax2.set_ylim(-20,20)
@@ -441,6 +444,137 @@ def plt_profile_wofit(samp,ax,ax1,ax2,ax3,RIN,ROUT,mv):
     ax3.xaxis.set_ticks([0.1,1,5,7])
     ax3.set_xticklabels([0.1,1,5,7])
     ax3.legend()
+
+
+def plt_profile_wofit_onlystandard(samp,ax,ax1,ax2,ax3,RIN,ROUT,mv):
+    
+    folder = '../../MICEv'+str(mv)+'.0/profiles/'
+
+
+    p_name = 'profile_'+samp+'.fits'
+    profile = fits.open(folder+p_name)
+
+    print(p_name)
+    
+    # '''
+    h   = profile[0].header
+    p   = profile[1].data
+    cov = profile[2].data
+    
+    cosmo = LambdaCDM(H0=100*h['hcosmo'], Om0=0.25, Ode0=0.75)
+    '''
+    
+    h = profile[1].header
+    p = profile[1].data
+    '''
+    micev = str(h['MICE version'])
+    
+    zmean = h['z_mean']
+    q  = h['q2d_mean']
+    qr = h['q2dr_mean']
+    
+    e = (1-q)/(1+q)
+    er = (1-qr)/(1+qr)
+    
+    H        = cosmo.H(zmean).value/(1.0e3*pc) #H at z_pair s-1 
+    roc      = (3.0*(H**2.0))/(8.0*np.pi*G) #critical density at z_pair (kg.m-3)
+    roc_mpc  = roc*((pc*1.0e6)**3.0)
+    
+    
+    ndots = p.shape[0]
+    
+    
+    GT  = p.GAMMA_Tcos
+    GTr = p.GAMMA_Tcos_reduced
+    GTc = p.GAMMA_Tcos_control
+    
+    GX  = p.GAMMA_Xsin
+    GXr = p.GAMMA_Xsin_reduced
+    GXc = p.GAMMA_Xsin_control
+    
+    # '''
+    CovDS  = cov.COV_ST.reshape(len(GT),len(GT))
+    
+    CovGT  = cov.COV_GT.reshape(len(GT),len(GT))
+    CovGTr = cov.COV_GT_reduced.reshape(len(GT),len(GT))
+    CovGTc = cov.COV_GT_control.reshape(len(GT),len(GT))
+    
+    CovGX  = cov.COV_GX.reshape(len(GT),len(GT))
+    CovGXr = cov.COV_GX_reduced.reshape(len(GT),len(GT))
+    CovGXc = cov.COV_GX_control.reshape(len(GT),len(GT))
+
+    # FIT MONOPOLE
+    rplot = np.arange(0.1,10,0.05)
+    
+    nfw    = Delta_Sigma_fit(p.Rp,p.DSigma_T,np.diag(CovDS),zmean,cosmo,True)
+    gt,gx   = GAMMA_components(rplot,zmean,ellip=e,M200 =nfw.M200,c200 = nfw.c200,cosmo=cosmo)
+    gtr,gxr = GAMMA_components(rplot,zmean,ellip=er,M200 =nfw.M200,c200 = nfw.c200,cosmo=cosmo)
+    
+    mass = str(np.round(np.log10(nfw.M200),1))
+    
+    
+        
+    ax.plot(p.Rp,p.DSigma_T,'C1')
+    ax.fill_between(p.Rp,p.DSigma_T+np.diag(CovDS),p.DSigma_T-np.diag(CovDS),color='C1',alpha=0.2)
+    ax.plot(nfw.xplot,nfw.yplot,'C3',label='fited nfw',alpha=0.5)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_ylabel(r'$\Delta\Sigma$')
+    ax.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax.set_ylim(1,200)
+    ax.set_xlim(0.1,10)
+    ax.xaxis.set_ticks([0.1,1,5,7])
+    ax.set_xticklabels([0.1,1,5,7])
+    ax.yaxis.set_ticks([1,10,100])
+    ax.set_yticklabels([1,10,100])
+
+    
+    # ax1.plot(RMt[0]*0.7,RMt[1]/0.7,'k',label='redMaPPer')
+    # ax1.errorbar(RMt[0]*0.7,RMt[1]/0.7,yerr=RMt[2]/0.7,fmt = 'none',ecolor='0.5')
+    
+    ax1.plot(p.Rp,GT,'C4',label = 'standard')
+    ax1.fill_between(p.Rp,GT+np.diag(CovGT),GT-np.diag(CovGT),color='C4',alpha=0.2)
+    ax1.plot(rplot,gt,'C3',alpha=0.5)
+    ax1.set_xscale('log')
+    ax1.set_yscale('log')
+    ax1.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax1.set_ylabel(r'$\Gamma_T$')
+    ax1.set_ylim(1,200)
+    ax1.set_xlim(0.1,10)
+    ax1.xaxis.set_ticks([0.1,1,5,7])
+    ax1.set_xticklabels([0.1,1,5,7])
+    ax1.yaxis.set_ticks([0.3,10,100])
+    ax1.set_yticklabels([0.3,10,100])
+    
+    # ax2.plot(RMt[0]*0.7,RMt[3]/0.7,'k',label='redMaPPer')
+    # ax2.errorbar(RMt[0]*0.7,RMt[3]/0.7,yerr=RMt[4]/0.7,fmt = 'none',ecolor='0.5')
+    
+    ax2.plot([0,5],[0,0],'C7')
+    ax2.plot(p.Rp,GX,'C2')
+    ax2.plot(rplot,gx,'C3',alpha=0.5)
+    ax2.fill_between(p.Rp,GX+np.diag(CovGX),GX-np.diag(CovGX),color='C2',alpha=0.2)
+    ax2.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax2.set_ylabel(r'$\Gamma_\times$')
+    ax2.set_xscale('log')
+    ax2.set_xlim(0.1,10)
+    ax2.set_ylim(-20,20)
+    ax2.xaxis.set_ticks([0.1,1,5,7])
+    ax2.set_xticklabels([0.1,1,5,7])
+    
+    
+    ax3.plot([0,5],[0,0],'C7')
+    ax3.plot(p.Rp,GTc,'k', label = 'GT control')
+    ax3.plot(p.Rp,GXc,'C8--', label = 'GX control')
+    ax3.fill_between(p.Rp,GXc+np.diag(CovGXc),GXc-np.diag(CovGXc),color='C8',alpha=0.2)
+    ax3.fill_between(p.Rp,GTc+np.diag(CovGTc),GTc-np.diag(CovGTc),color='C7',alpha=0.2)
+    ax3.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax3.set_xscale('log')
+    ax3.set_xlim(0.1,10)
+    ax3.set_ylim(-10,7)
+    ax3.xaxis.set_ticks([0.1,1,5,7])
+    ax3.set_xticklabels([0.1,1,5,7])
+
+
 
 
 def plt_profile(samp,ax,ax1,ax2,ax3,RIN,ROUT,mv):
@@ -556,6 +690,7 @@ def plt_profile(samp,ax,ax1,ax2,ax3,RIN,ROUT,mv):
     ax.plot(rplot,DS_GX,'C2--',label='mcmc gx')
     ax.set_xscale('log')
     ax.set_yscale('log')
+    ax.set_ylabel(r'$\Delta\Sigma$')
     ax.set_xlabel('r [$h^{-1}$ Mpc]')
     ax.set_ylim(2,200)
     ax.set_xlim(0.1,5)
@@ -584,6 +719,7 @@ def plt_profile(samp,ax,ax1,ax2,ax3,RIN,ROUT,mv):
     ax1.set_xscale('log')
     ax1.set_yscale('log')
     ax1.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax1.set_ylabel(r'$\Gamma_T$')
     ax1.set_ylim(1,100)
     ax1.set_xlim(0.1,5)
     ax1.xaxis.set_ticks([0.1,1,3])
@@ -608,6 +744,7 @@ def plt_profile(samp,ax,ax1,ax2,ax3,RIN,ROUT,mv):
     ax2.fill_between(p.Rp,GX+np.diag(CovGX),GX-np.diag(CovGX),color='C2',alpha=0.2)
     ax2.fill_between(p.Rp,GXr+np.diag(CovGXr),GXr-np.diag(CovGXr),color='C5',alpha=0.2)
     ax2.set_xlabel('r [$h^{-1}$ Mpc]')
+    ax2.set_ylabel(r'$\Gamma_\times$')
     ax2.set_xscale('log')
     ax2.set_xlim(0.1,5)
     ax2.set_ylim(-10,10)
@@ -627,7 +764,9 @@ def plt_profile(samp,ax,ax1,ax2,ax3,RIN,ROUT,mv):
     ax3.xaxis.set_ticks([0.1,1,3])
     ax3.set_xticklabels([0.1,1,3])
     ax3.legend()
-
+    
+    
+'''
 folder = '../../MICEv2.0/profiles/'
 
 f, ax = plt.subplots()
@@ -651,7 +790,7 @@ f3.savefig(folder+'comparison-MICE/'+'control'+samp+'.png')
 
 
 
-'''
+
 ft, axt  = plt.subplots(2,4, figsize=(12,6), sharey=True)
 ft1, axt1 = plt.subplots(2,4, figsize=(12,6), sharey=True)
 ft2, axt2 = plt.subplots(2,4, figsize=(12,6), sharey=True)

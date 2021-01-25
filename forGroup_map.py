e91df02ab47612a8bf57572c65e7b7afbdce5f19
eli <elizabeth.johana.gonzalez@gmail.com>
2021-01-25 18:30:21 -0300

    add map

--- forGroup_map.py
+++ forGroup_map.py
@@ -244,7 +244,7 @@ def main(sample='pru', rprox = 'Rprox_lM14cut',
         print(z_min,' <= z < ',z_max)
         print(q_min,' <= q < ',q_max)
         print('Profile has ',ndots,'bins')
-        print('from ',RIN,'kpc to ',ROUT,'kpc')
+        print('max',ROUT,'kpc')
         print('h ',hcosmo)
         print('MICE version ',vmice)
               

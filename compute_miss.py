55f3a22a39fec21da26d473884d3a95be3bccd25
eli <elizabeth.johana.gonzalez@gmail.com>
2021-01-28 09:27:27 -0300

    compute map

--- compute_miss.py
+++ compute_miss.py
@@ -15,15 +15,15 @@ Msun = M_sun.value # Solar mass (kg)
 
 
     
-folder = '/home/elizabeth/MICEv2.0/'
+folder = '/mnt/clemente/lensing/HALO_SHAPE/MICE_v2.0/catalogs/'
 
 
-p_name = 'profiles/profile_ebin_142.fits'
-m_name = 'mapas/mapa_bin_144.fits'
+p_name = 'profile_ebin_142.fits'
+m_name = 'mapa_bin_142.fits'
 
 profile = fits.open(folder+p_name)
 mapa = fits.open(folder+m_name)[1].data
-fitmiss = fits.open(folder+'profiles/fitresults_mono_Rayleigh_0_2500_profile_ebin_142.fits')[0].header
+fitmiss = fits.open(folder+'fitresults_mono_Rayleigh_0_2500_profile_ebin_142.fits')[0].header
 
 print(p_name)
 
@@ -53,11 +53,14 @@ theta  = np.arctan2(y,x)
 r = np.sqrt(x**2 + y**2)
 
 
-DS = Delta_Sigma_NFW_miss_parallel(r,zmean,10**lM200_miss,s_off = soff,c200 = c200_miss, P_Roff= Rayleigh, cosmo=cosmo,ncores=40)
-gt,gx = GAMMA_components_miss_parallel(r,zmean,10**lM200_miss,ellip=e,s_off = soff,c200 = c200_miss, P_Roff= Rayleigh, cosmo=cosmo,ncores=40)
+DS = Delta_Sigma_NFW_miss_parallel(r,zmean,10**lM200_miss,s_off = soff,c200 = c200_miss, P_Roff= Rayleigh, cosmo=cosmo,ncores=56)
+S = Sigma_NFW_miss_parallel(r,zmean,10**lM200_miss,s_off = soff,c200 = c200_miss, P_Roff= Rayleigh, cosmo=cosmo,ncores=56)
+gt,gx = GAMMA_components_miss_parallel(r,zmean,10**lM200_miss,ellip=e,s_off = soff,c200 = c200_miss, P_Roff= Rayleigh, cosmo=cosmo,ncores=56)
 
-table = [fits.Column(name='xmpc', format='E', array=x),
+table = [fits.Column(name='rmpc', format='E', array=r),
+            fits.Column(name='xmpc', format='E', array=x),
             fits.Column(name='ympc', format='E', array=y),
+            fits.Column(name='S', format='E', array=S),
             fits.Column(name='DS', format='E', array=DS),
             fits.Column(name='Gt', format='E', array=gt),
             fits.Column(name='Gx', format='E', array=gx)]
@@ -71,4 +74,4 @@ primary_hdu = fits.PrimaryHDU(header=h)
 
 hdul = fits.HDUList([primary_hdu, tbhdu])
 
-hdul.writeto(folder+'mapas/mapa_bin_142_miss.fits',overwrite=True)
+hdul.writeto(folder+'mapa_bin_142_miss.fits',overwrite=True)

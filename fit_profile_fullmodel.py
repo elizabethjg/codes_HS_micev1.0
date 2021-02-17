b3d7c5077c5f13ece27478718d9706cbac273162
eli <elizabeth.johana.gonzalez@gmail.com>
2021-02-17 11:05:55 -0300

    fit model

--- fit_profile_fullmodel.py
+++ fit_profile_fullmodel.py
@@ -28,7 +28,7 @@ ROUT =2000.
 # '''
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-folder', action='store', dest='folder', default='/mnt/clemente/lensing/HALO_SHAPE/MICE_v2.0/profiles/')
+parser.add_argument('-folder', action='store', dest='folder', default='/mnt/clemente/lensing/HALO_SHAPE/MICEv2.0/profiles/')
 parser.add_argument('-file', action='store', dest='file_name', default='profile.cat')
 parser.add_argument('-ncores', action='store', dest='ncores', default=40)
 parser.add_argument('-RIN', action='store', dest='RIN', default=0)

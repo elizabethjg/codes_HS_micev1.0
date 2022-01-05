181767172932f8978fc51d4462af5167fb08c9fc
elizabethjg <elizabet.johana.gonzalez@gmail.com>
2022-01-05 16:35:50 -0300

    run fit

--- script_sersic.sh
+++ script_sersic.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
-python -u forGroup_qprofile.py -sample '136_145' -lM_min 13.6 -lM_max 14.5 -z_max 0.12 -ncores 30
-python -u forGroup_qprofile_individual.py -sample '136_145' -lM_min 13.6 -lM_max 14.5 -z_max 0.12 -ncores 30
-python -u forGroup_map.py -sample '136_145' -lM_min 13.6 -lM_max 14.5 -z_max 0.12 -ncores 30
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Lz.fits -RIN 250 -ROUT 200 -ncores 32
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Hz.fits -RIN 250 -ROUT 200 -ncores 32
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Lz.fits -RIN 250 -ROUT 200 -ncores 32
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Hz.fits -RIN 250 -ROUT 200 -ncores 32
+##
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Lz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Hz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Lz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Hz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32
+##
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Lz.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Hz.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Lz.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Hz.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+##
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Lz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Hz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Lz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+python fit_allprofiles_centred_withoutc200.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Hz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+##
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Lz.fits -RIN 250 -ROUT 200 -ncores 32
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Hz.fits -RIN 250 -ROUT 200 -ncores 32
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Lz.fits -RIN 250 -ROUT 200 -ncores 32
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Hz.fits -RIN 250 -ROUT 200 -ncores 32
+##
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Lz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Hz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Lz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Hz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32
+##
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Lz.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Hz.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Lz.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Hz.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+##
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Lz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_HM_Hz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Lz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'
+python fit_allprofiles_centred.py -folder '/home/elizabeth/MICE/HS-lensing/profiles' -file profile_LM_Hz_relaxed.fits -RIN 250 -ROUT 200 -ncores 32 -ang 'reduced'

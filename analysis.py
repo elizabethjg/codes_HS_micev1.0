a6658f2fede301fc6d208c7283f2b87f89c13c4e
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2022-04-24 11:34:57 -0300

    update

--- analysis.py
+++ analysis.py
@@ -578,7 +578,7 @@ cstyle_ext = ['C1^','C1v','C3^','C3v','C5^','C5v']
 ROUToq_ext = ['2000','1000','2000','1000','2000','1000']
 RIN_mix00 = ['200','200','300','300','400','400']
 RIN_mix50 = ['250','250','350','350','450','450']
-RIN_mix = ['250','250','300','400','400','400']
+RIN_mix = ['250','250','300','400','400','40 	0']
 RIN_mix350 = ['350','350','350','350','350','350']
 
 
@@ -594,7 +594,7 @@ hsamps_ext_rel = ['HM_Lz_relaxed','LM_Lz_relaxed',
 # plot_bias(hsamps_mix2,lhs_ext,cstyle_ext,'mix00',RIN_mix00,ROUToq_ext,False)
 # plot_bias(hsamps_mix2,lhs_ext,cstyle_ext,'mix50',RIN_mix50,ROUToq_ext,False)
 # plot_bias(hsamps_mix2,lhs_ext,cstyle_ext,'mix350',RIN_mix350,ROUToq_ext,False)
-plot_bias(hsamps_mix2,lhs_ext,cstyle_ext,'mix',RIN_mix,ROUToq_ext,False)
+##plot_bias(hsamps_mix2,lhs_ext,cstyle_ext,'mix',RIN_mix,ROUToq_ext,False)
 # '''
 # plot_bias(hsamps_nr,lhs,cstyle,'nonrex_comprel_samps',250,ROUToq,True)
 # plot_bias(hsamps_ext,lhs_ext,cstyle_ext,'final',350,ROUToq_ext,False)
@@ -629,3 +629,16 @@ ax_all[0,1].legend(loc=3,frameon=False)
     
 f.savefig(folder+'../test_plots/profile_relaxed_Ein.png',bbox_inches='tight')
 '''
+
+f, ax_all = plt.subplots(1,3, figsize=(16,5),sharex = True)
+f.subplots_adjust(hspace=0)
+
+plt_profile_fitted_final('HM_Lz',250,5000,ax_all,fittype='_2h_2q')
+
+
+ax_all[0,0].legend(loc=3,frameon=False)
+ax_all[0,1].legend(loc=3,frameon=False)
+
+
+    
+f.savefig(folder+'../test_plots/final/profile_HM_Lz.png',bbox_inches='tight')

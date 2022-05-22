5d3efd97cd743db81421f3d3f9ae48473ecff9a7
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2022-05-22 17:21:24 -0300

    run mis10

--- run_fit_forz_400.sh
+++ run_fit_forz_400.sh
@@ -4,28 +4,28 @@ source /home/elizabeth/.bashrc
 
 conda activate myenv
 
-python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Lz.fits -RIN 350 -ROUT 1000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Lz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Lz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Lz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Lz.fits -RIN 350 -ROUT 2000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Lz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Lz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Lz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Mz.fits -RIN 350 -ROUT 2000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Mz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Mz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Mz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Hz.fits -RIN 400 -ROUT 1000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Hz.fits -RIN 400 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Hz.fits -RIN 400 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Hz.fits -RIN 400 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Hz.fits -RIN 450 -ROUT 2000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Hz.fits -RIN 450 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Hz.fits -RIN 450 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Hz.fits -RIN 450 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Mz.fits -RIN 350 -ROUT 1000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Mz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Mz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
-python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Mz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 &
+python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Lz.fits -RIN 350 -ROUT 1000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Lz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Lz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Lz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Lz.fits -RIN 350 -ROUT 2000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Lz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Lz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Lz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Mz.fits -RIN 350 -ROUT 2000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Mz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Mz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Mz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Hz.fits -RIN 400 -ROUT 1000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Hz.fits -RIN 400 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Hz.fits -RIN 400 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Hz.fits -RIN 400 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Hz.fits -RIN 450 -ROUT 2000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Hz.fits -RIN 450 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Hz.fits -RIN 450 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Hz.fits -RIN 450 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Mz.fits -RIN 350 -ROUT 1000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Mz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Mz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
+python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Mz.fits -RIN 350 -ROUT 5000 -ncores 7 -nit 250 -qext _mis10 -ang reduced &
 wait

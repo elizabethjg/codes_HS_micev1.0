964c3ae451ef8b3897d42de370c11755701a74a9
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2022-05-27 08:48:21 -0300

    fit 1

--- run_fit_forz_400.sh
+++ run_fit_forz_400.sh
@@ -12,8 +12,8 @@ python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing
 python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Lz_relaxed.fits -RIN 350 -ROUT 5000 -ncores 8 -nit 250 &
 python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Lz_relaxed.fits -RIN 350 -ROUT 5000 -ncores 8 -nit 250 &
 python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Lz_relaxed.fits -RIN 350 -ROUT 5000 -ncores 8 -nit 250 &
-python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Mz_relaxed.fits -RIN 350 -ROUT 2000 -ncores 8 -nit 250 &
-python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Mz_relaxed.fits -RIN 350 -ROUT 5000 -ncores 8 -nit 250 &
-python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Mz_relaxed.fits -RIN 350 -ROUT 5000 -ncores 8 -nit 250 &
-python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_HM_Mz_relaxed.fits -RIN 350 -ROUT 5000 -ncores 8 -nit 250 &
+python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Mz_relaxed.fits -RIN 350 -ROUT 1000 -ncores 8 -nit 250 &
+python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Mz_relaxed.fits -RIN 350 -ROUT 5000 -ncores 8 -nit 250 &
+python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Mz_relaxed.fits -RIN 350 -ROUT 5000 -ncores 8 -nit 250 &
+python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_LM_Mz_relaxed.fits -RIN 350 -ROUT 5000 -ncores 8 -nit 250 &
 wait

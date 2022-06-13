4b1e6e8ea561cffd2901c2533a3a7487b8cbbf96
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2022-06-13 18:10:24 -0300

    run for relaxed

--- run_fit_forz.sh
+++ run_fit_forz.sh
@@ -4,12 +4,12 @@ source /home/elizabeth/.bashrc
 
 conda activate myenv
 
-python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_all.fits      -RIN 350 -ROUT 1500 -ncores 6 -nit 250 -ang reduced&
-python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_all.fits      -RIN 350 -ROUT 5000 -ncores 6 -nit 250 -ang reduced&
-python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_all.fits      -RIN 350 -ROUT 5000 -ncores 6 -nit 250 -ang reduced&
-python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_all.fits      -RIN 350 -ROUT 5000 -ncores 6 -nit 250 -ang reduced&
-python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_all_qmed.fits -RIN 350 -ROUT 1500 -ncores 6 -nit 250 -ang reduced&
-python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_all_qmed.fits -RIN 350 -ROUT 5000 -ncores 6 -nit 250 -ang reduced&
-python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_all_qmed.fits -RIN 350 -ROUT 5000 -ncores 6 -nit 250 -ang reduced&
-python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_all_qmed.fits -RIN 350 -ROUT 5000 -ncores 6 -nit 250 -ang reduced&
+python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_all_relaxed.fits      -RIN 350 -ROUT 1500 -ncores 6 -nit 250 &
+python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_all_relaxed.fits      -RIN 350 -ROUT 5000 -ncores 6 -nit 250 &
+python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_all_relaxed.fits      -RIN 350 -ROUT 5000 -ncores 6 -nit 250 &
+python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_all_relaxed.fits      -RIN 350 -ROUT 5000 -ncores 6 -nit 250 &
+python fit_allprofiles_centred_sequential.py           -folder ~/MICE/HS-lensing/profiles/ -file profile_all_qmin_relaxed.fits -RIN 350 -ROUT 1500 -ncores 6 -nit 250 &
+python fit_allprofiles_centred_sequential_2h_2q_Ein.py -folder ~/MICE/HS-lensing/profiles/ -file profile_all_qmin_relaxed.fits -RIN 350 -ROUT 5000 -ncores 6 -nit 250 &
+python fit_allprofiles_centred_sequential_2h_2q_woc.py -folder ~/MICE/HS-lensing/profiles/ -file profile_all_qmin_relaxed.fits -RIN 350 -ROUT 5000 -ncores 6 -nit 250 &
+python fit_allprofiles_centred_sequential_2h_2q.py     -folder ~/MICE/HS-lensing/profiles/ -file profile_all_qmin_relaxed.fits -RIN 350 -ROUT 5000 -ncores 6 -nit 250 &
 wait
